1d6245b1ea0b113cf5d0f6939a784a794baae906
Massimo <massimo.prencipe@solita.fi>
2019-09-20 09:14:56 +0300

    DPO-798 retain history data

--- dbmarine/sql/update/V2/V2.2.5__DPO-798_bridgelock_disruptions.sql
+++ dbmarine/sql/update/V2/V2.2.5__DPO-798_bridgelock_disruptions.sql
@@ -1,13 +1,14 @@
 CREATE TABLE BRIDGELOCK_DISRUPTION(
-    bridgelock_id BIGINT PRIMARY KEY NOT NULL,
-    bridgelock_type_id BIGINT NOT NULL,
-    start_date TIMESTAMP(0) WITH TIME ZONE NOT NULL,
-    end_date TIMESTAMP(0) WITH TIME ZONE NOT NULL,
+    bridgelock_id BIGINT,
+    bridgelock_type_id BIGINT,
+    start_date TIMESTAMP(0) WITH TIME ZONE,
+    end_date TIMESTAMP(0) WITH TIME ZONE,
     geometry GEOMETRY, -- requires PostGIS
     description_fi TEXT,
     description_sv TEXT,
     description_en TEXT,
     additional_info_fi TEXT,
     additional_info_sv TEXT,
-    additional_info_en TEXT
+    additional_info_en TEXT,
+    PRIMARY KEY (bridgelock_id, bridgelock_type_id, start_date, end_date)
 );
