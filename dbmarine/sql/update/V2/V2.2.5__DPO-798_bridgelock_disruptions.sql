c2d57fde364443f8530b4b3d3c3c57df773122d7
Massimo <massimo.prencipe@solita.fi>
2019-09-20 16:16:43 +0300

    DPO-798 allow updates to end date, remove redundant index creation (created by postgresql)

--- dbmarine/sql/update/V2/V2.2.5__DPO-798_bridgelock_disruptions.sql
+++ dbmarine/sql/update/V2/V2.2.5__DPO-798_bridgelock_disruptions.sql
@@ -2,7 +2,7 @@ CREATE TABLE BRIDGELOCK_DISRUPTION(
     bridgelock_id BIGINT,
     bridgelock_type_id BIGINT,
     start_date TIMESTAMP(0) WITH TIME ZONE,
-    end_date TIMESTAMP(0) WITH TIME ZONE,
+    end_date TIMESTAMP(0) WITH TIME ZONE NOT NULL,
     geometry GEOMETRY, -- requires PostGIS
     description_fi TEXT,
     description_sv TEXT,
@@ -10,7 +10,5 @@ CREATE TABLE BRIDGELOCK_DISRUPTION(
     additional_info_fi TEXT,
     additional_info_sv TEXT,
     additional_info_en TEXT,
-    PRIMARY KEY (bridgelock_id, bridgelock_type_id, start_date, end_date)
+    PRIMARY KEY (bridgelock_id, bridgelock_type_id, start_date)
 );
-
-CREATE UNIQUE INDEX bridgelock_primary_key ON BRIDGELOCK_DISRUPTION (bridgelock_id, bridgelock_type_id, start_date, end_date);
